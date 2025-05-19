42b3b238e6b190fc0eee18f27e0b52e174928ca1
EstebanGC <johan.gal.car@gmail.com>
2025-05-18 20:56:13 -0500

    whatever

---
+++ common-events/src/main/java/com/model/UserCreatedEvent.java
@@ -0,0 +1,9 @@
+package main.java.com.model;
+
+import java.time.Instant;
+
+public class UserCreatedEvent {
+    private String userId;
+    private String email;
+    private Instant createdAt;
+}
