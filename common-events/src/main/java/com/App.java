c6bc78d54f8163adf595b97ff38d8848d694f04a
EstebanGC <johan.gal.car@gmail.com>
2025-05-24 21:51:52 -0500

    .

--- common-events/src/main/java/com/App.java
+++ common-events/src/main/java/com/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Common events!"+"Whatever and whenever" );
+        System.out.println( "Common events!"+"Whatever and whenever, whoever" );
     }
 }
