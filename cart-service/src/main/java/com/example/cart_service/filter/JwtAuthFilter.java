7fd9861c379658d9b9171e6d1341850f66915eb5
EstebanGC <johan.gal.car@gmail.com>
2025-07-20 19:12:56 -0500

    Adding JwtService and JwtAuthFilter

---
+++ cart-service/src/main/java/com/example/cart_service/filter/JwtAuthFilter.java
@@ -0,0 +1,13 @@
+package com.example.cart_service.filter;
+
+import com.example.cart_service.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+}
