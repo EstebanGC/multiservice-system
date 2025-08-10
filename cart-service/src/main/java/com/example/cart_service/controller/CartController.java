efa1658f55ca943d50d01567f22f5932e152c647
EstebanGC <johan.gal.car@gmail.com>
2025-08-09 19:41:13 -0500

    .

--- cart-service/src/main/java/com/example/cart_service/controller/CartController.java
+++ cart-service/src/main/java/com/example/cart_service/controller/CartController.java
@@ -2,11 +2,14 @@ package com.example.cart_service.controller;
 
 import com.example.cart_service.dto.AddToCartRequest;
 import com.example.cart_service.service.CartService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/cart")
@@ -16,8 +19,16 @@ public class CartController {
     private final CartService cartService;
 
     @PostMapping("/add")
-    public ResponseEntity<?> addToCart(@RequestBody @Valid AddToCartRequest request) {
-        String sessionId = request.getSessionId();  // o de una cookie
+    public ResponseEntity<?> addToCart(
+            @RequestBody @Valid AddToCartRequest request,
+            @CookieValue(value = "SESSIONID", required = false) String sessionId,
+            HttpServletResponse response
+    ) {
+        if (sessionId == null) {
+            sessionId = UUID.randomUUID().toString();
+            response.addHeader("Set-Cookie", "SESSIONID=" + sessionId + "; Path=/; HttpOnly");
+        }
+
         cartService.addToCart(sessionId, request);
         return ResponseEntity.ok().build();
     }
