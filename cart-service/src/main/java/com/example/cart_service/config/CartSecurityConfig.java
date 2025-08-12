112194e8fb3ec08f21a74d1c74a6a9ee60fce211
EstebanGC <johan.gal.car@gmail.com>
2025-08-11 22:19:29 -0500

    Deleting unnecessary dependencies

--- cart-service/src/main/java/com/example/cart_service/config/CartSecurityConfig.java
+++ cart-service/src/main/java/com/example/cart_service/config/CartSecurityConfig.java
@@ -1,18 +1,10 @@
 package com.example.cart_service.config;
 
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.crypto.spec.SecretKeySpec;
-import java.util.Base64;
 
 @Configuration
 @EnableWebSecurity
--- cart-service/src/main/java/com/example/cart_service/controller/CartController.java
+++ cart-service/src/main/java/com/example/cart_service/controller/CartController.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
